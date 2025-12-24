7bfeec8c9cd18535f139682b22a552ffb2230c37
Md Nesad <nesadm26@gmail.com>
2025-12-24 23:30:23 +0600

    problem solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import DashboardRouter from "./routers/dashboard/DashboardRouter";
 
 export default function App() {
+  // if (admin.localhost) return <h1>Admin Panel</h1>;
   return (
     <>
       <DashboardRouter />
