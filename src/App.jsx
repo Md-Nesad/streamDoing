1d1babafc0203ea0f174259b8eb99ae81be209d7
Md Nesad <nesadm26@gmail.com>
2025-12-27 23:43:30 +0600

    API integration start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import DashboardRouter from "./routers/dashboard/DashboardRouter";
 
 export default function App() {
-  // if (admin.localhost) return <h1>Admin Panel</h1>;
   return (
     <>
       <DashboardRouter />
