887e46d3d05ec1e99238d9d6f7e26cebd40d57b5
Md Nesad <nesadm26@gmail.com>
2026-01-01 02:07:40 +0600

    added delete functionality for the agencies

--- src/components/agencies/AddMasterAgencyForm.jsx
+++ src/components/agencies/AddMasterAgencyForm.jsx
@@ -145,6 +145,7 @@ export default function AddMasterAgencyForm() {
                 {...register("country")}
                 className="w-full border border-gray-300 rounded-md px-3 py-2 mt-1 text-sm appearance-none"
               >
+                <option value="">Select</option>
                 {countries?.map((country) => (
                   <option key={country._id} value={country._id}>
                     {country.name}
