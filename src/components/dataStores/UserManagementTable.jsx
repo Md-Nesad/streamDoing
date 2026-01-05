fbafba2d0f98eb033e18e09d8262332315d5ca7a
Md Nesad <nesadm26@gmail.com>
2026-01-05 20:09:54 +0600

    data store api's functionallity complete

--- src/components/dataStores/UserManagementTable.jsx
+++ src/components/dataStores/UserManagementTable.jsx
@@ -1,21 +1,63 @@
 import { Funnel, Pen, Trash2 } from "lucide-react";
-import { usersTable } from "../../data/data";
-import { useState } from "react";
-import EditUserDetailsModal from "../../modals/dataSroreModals/EditUserDetailsModal";
+import { useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import Loading from "../Loading";
 import Error from "../Error";
 import { BASE_URL } from "../../utility/utility";
+import { useNavigate } from "react-router-dom";
+import Pagination from "../Pagination";
+import useDelete from "../../hooks/useDelete";
 
 export default function UserManagementTable() {
-  const [isOpen, setIsOpen] = useState(false);
-  // const navigate = useNavigate();
-  // const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+  const [text, setText] = useState("");
   const [page, setPage] = useState(1);
   const { data, loading, error } = useFetch(
     `${BASE_URL}/admin/users?page=${page}&limit=20`
   );
-  const users = data?.users || [];
+  const [users, setUsers] = useState(data?.users || []);
+  const pagination = data?.pagination;
+  const deleteUser = useDelete(`${BASE_URL}/admin/users`);
+
+  //handle filter
+  const handleFilter = () => {
+    const filteredUsers = data?.users?.filter((user) => {
+      return (
+        user.name.toLowerCase().includes(text.toLowerCase()) ||
+        user.displayId.toString().includes(text)
+      );
+    });
+    setUsers(filteredUsers);
+  };
+
+  //handle delete
+  const handleDelete = async (id) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this user?"
+    );
+    if (!confirmDelete) return;
+
+    const result = await deleteUser(id);
+
+    if (!result) {
+      alert("Failed to delete user");
+    } else {
+      alert(result.message);
+    }
+    //fetching data after delete
+    setUsers((prev) => prev.filter((user) => user._id !== id));
+  };
+
+  useEffect(() => {
+    setUsers(data?.users || []);
+  }, [data]);
+
+  useEffect(() => {
+    if (text === "") {
+      handleFilter();
+    }
+  }, [text]);
+
   if (loading) return <Loading />;
   if (error) return <Error error={error} />;
   return (
@@ -25,18 +67,26 @@ export default function UserManagementTable() {
         {/* Search Input */}
         <input
           type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           className="border border-[#BBBBBB] outline-[#BBBBBB] w-full sm:max-w-[75%] px-4 py-1.5 rounded-md"
           placeholder="Search by ID or name"
         />
 
         {/* Buttons */}
         <div className="flex items-center justify-end gap-2 sm:gap-3 w-full sm:w-auto">
-          <button className="px-3 sm:px-4 py-1.5 rounded-md bg-white border border-[#CCCCCC] font-medium flex items-center justify-center gap-2 text-sm sm:text-base w-full sm:w-auto">
+          <button
+            onClick={handleFilter}
+            className="px-3 sm:px-4 py-1.5 rounded-md bg-white border border-[#CCCCCC] font-medium flex items-center justify-center gap-2 text-sm sm:text-base w-full sm:w-auto"
+          >
             <Funnel size={18} /> Filter
           </button>
-          {/* <button className="px-3 sm:px-6 py-1.5 text-sm sm:text-base bg-linear-to-r from-[#6DA5FF] to-[#F576D6] text-white rounded-md font-medium w-full sm:w-auto text-nowrap">
+          <button
+            onClick={() => navigate("/dashboard/agencies/add-host-agency")}
+            className="px-3 sm:px-6 py-1.5 text-sm sm:text-base bg-linear-to-r from-[#6DA5FF] to-[#F576D6] text-white rounded-md font-medium w-full sm:w-auto text-nowrap"
+          >
             Add Agency
-          </button> */}
+          </button>
         </div>
       </div>
 
@@ -58,52 +108,65 @@ export default function UserManagementTable() {
           </thead>
 
           <tbody>
-            {users?.map((user, index) => (
-              <tr
-                key={index}
-                className="border-t border-[#DFDFDF] hover:bg-gray-50 text-md"
-              >
-                <td className="p-3 font-medium pl-5">{user.displayId}</td>
-                <td className="p-3">{user.name}</td>
-                <td className="p-3">{user.gender || "N/A"}</td>
-                <td className="p-3">{user.email}</td>
-                <td className="p-3">
-                  <span className="px-1.5 py-1 text-xs bg-linear-to-b from-[#5DB90A] to-[#175111] rounded-lg font-semibold text-white flex items-center gap-1.5 w-10 pl-3">
-                    {/* <img src={star} alt="" className="w-4 h-4" /> Lv */}
-                    LV{user.level || "N/A"}
-                  </span>
-                </td>
-                <td className="p-3">{user.phone}</td>
-                <td className="p-3">{user.location || "N/A"}</td>
-                <td className="p-3">
-                  <span
-                    className={`px-4 py-1 text-xs ${
-                      user.status === "active"
-                        ? "bg-linear-to-r from-[#79D49B] to-[#25C962]"
-                        : "bg-[#FF929296] text-[#D21B20]"
-                    } text-[#005D23] rounded-full font-semibold`}
-                  >
-                    {user.status}
-                  </span>
-                </td>
-                <td className="p-3 mt-1.5 text-[#181717] text-sm font-medium cursor-pointer flex gap-5 items-center">
-                  <span className="flex items-center gap-3">
-                    <button onClick={() => setIsOpen(true)} title="Edit">
-                      <Pen size={19} />
-                    </button>
-                    <Trash2 size={18} className="text-[#FF0037]" />
-                  </span>
+            {users?.length > 0 ? (
+              users?.map((user, index) => (
+                <tr
+                  key={index}
+                  className="border-t border-[#DFDFDF] hover:bg-gray-50 text-md"
+                >
+                  <td className="p-3 font-medium pl-5">{user.displayId}</td>
+                  <td className="p-3">{user.name}</td>
+                  <td className="p-3">{user.gender || "N/A"}</td>
+                  <td className="p-3">{user.email}</td>
+                  <td className="p-3">
+                    <span className="px-1.5 py-1 text-xs bg-linear-to-b from-[#5DB90A] to-[#175111] rounded-lg font-semibold text-white flex items-center gap-1.5 w-10 pl-3">
+                      {/* <img src={star} alt="" className="w-4 h-4" /> Lv */}
+                      LV{user.level || "N/A"}
+                    </span>
+                  </td>
+                  <td className="p-3">{user.phone}</td>
+                  <td className="p-3">{user.location || "N/A"}</td>
+                  <td className="p-3">
+                    <span
+                      className={`px-4 py-1 text-xs ${
+                        user.status === "active"
+                          ? "bg-linear-to-r from-[#79D49B] to-[#25C962]"
+                          : "bg-[#FF929296] text-[#D21B20]"
+                      } text-[#005D23] rounded-full font-semibold`}
+                    >
+                      {user.status}
+                    </span>
+                  </td>
+                  <td className="p-3 mt-1.5 text-[#181717] text-sm font-medium cursor-pointer flex gap-5 items-center">
+                    <span className="flex items-center gap-3">
+                      <button onClick={() => setIsOpen(true)} title="Edit">
+                        <Pen size={19} />
+                      </button>
+                      <button
+                        onClick={() => handleDelete(user._id)}
+                        title="Delete"
+                      >
+                        <Trash2 size={18} className="text-[#FF0037]" />
+                      </button>
+                    </span>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr className="border-t border-[#DFDFDF] hover:bg-gray-50 text-md mt-10">
+                <td colSpan={9} className="p-3 text-center">
+                  No user found
                 </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
-        {isOpen && (
-          <EditUserDetailsModal
-            open={isOpen}
-            onClose={() => setIsOpen(false)}
-          />
-        )}
+        <Pagination
+          page={page}
+          limit={pagination?.limit}
+          total={pagination?.total}
+          onPageChange={setPage}
+        />
       </div>
     </>
   );
