f5694b15d60651be73cb01dd8f81a6ad008b380d
Md Nesad <nesadm26@gmail.com>
2026-01-18 14:25:44 -0600

    zigo log reduced

--- src/zego/ZegoClient.js
+++ src/zego/ZegoClient.js
@@ -1,5 +1,10 @@
 import { ZegoExpressEngine } from "zego-express-engine-webrtc";
-
 import { ZEGO_VIDEO_APP_ID, ZEGO_VIDEO_APP_SIGN } from "../utility/utility";
 
 export const zg = new ZegoExpressEngine(ZEGO_VIDEO_APP_ID, ZEGO_VIDEO_APP_SIGN);
+
+zg.setLogConfig({
+  logLevel: "error",
+});
+
+zg.setDebugVerbose(false);
