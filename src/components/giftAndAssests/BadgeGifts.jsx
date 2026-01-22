3d8b2632505d630bfebb1b7a8b6cafcda3f28a79
Md Nesad <nesadm26@gmail.com>
2026-01-22 05:37:16 -0600

    added level config modal

---
+++ src/components/giftAndAssests/BadgeGifts.jsx
@@ -0,0 +1,8 @@
+export default function BadgeGifts() {
+  return (
+    <div className="bg-white rounded p-4">
+      <h3 className="font-semibold mb-3">Badge Gifts</h3>
+      {/* Badge gift table */}
+    </div>
+  );
+}
---
+++ src/components/giftAndAssests/BannerGifts.jsx
@@ -0,0 +1,8 @@
+export default function BannerGifts() {
+  return (
+    <div className="bg-white rounded p-4">
+      <h3 className="font-semibold mb-3">Banner Gifts</h3>
+      {/* Banner gift table */}
+    </div>
+  );
+}
